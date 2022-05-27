97d2ed439814b21ce793c8c357f375350c9e3bdf
ArnoudvD <106403324+ArnoudvD@users.noreply.github.com>
2022-05-27 21:07:06 +0200

    Create scraper_adom.py

---
+++ .idea/scraper_adom.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+url = 'https://www.evenementkalender.nl/2022-05'
+response = requests.get(url)
+text = response.text
+data = BeautifulSoup(text, 'html.parser')
+
+help(data)
+
+# headings = data.find_all('tr')[0]
+# headings_list = []  # list to store all headings
+#
+# for x in headings:
+#     headings_list.append(x.text)
+#
+# print(headings_list[:10])
